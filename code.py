a00874d9095c50e224e584e5f3495c50ed88bd04
Christelle de Boutray <christelle.de_boutray@etu.minesparis.psl.eu>
2024-10-29 15:53:58 +0100

    merge corrige

--- code.py
+++ code.py
@@ -1,7 +1,22 @@
-# +
-import pandas as pd
+# # Projet Hackathon - Christelle, Maxime, Jérémie, Pierre-Louis
+#
+# * Projet choisi _Le bonheur_
+# * Objectif : définition d'un nouvelle indice de bonheur
+
+# ## Importation des librairies
+
+import matplotlib.pyplot as plt 
+import pandas as pd 
 import numpy as np
+import common as c
 import seaborn as sns
+
+df = pd.read_csv('bonheur.csv', sep=';')
+df.head(2)
+
+df['GDP Normalisé'] = df['Log GDP per capita']/(df.loc[df['Log GDP per capita'].argmax(), 'Log GDP per capita'])
+df.head(3)
+
 
 df=pd.read_csv('bonheur.csv',sep=';')
 display(df.head(2))
@@ -10,7 +25,7 @@ display(df.columns)
 
 
 
-# +
+
 df['christelle']=df['Social support']+df['Generosity']
 df['Generosity'].describe
 
@@ -19,7 +34,7 @@ df['Healthy life expectancy at birth']=pd.to_numeric(df['Healthy life expectancy
 df['christelle']=df['Social support']+df['Generosity']+df['Healthy life expectancy at birth']*0.05+df['Freedom to make life choices']-df['Perceptions of corruption']
 df.head(5)
 
-# +
+
 country=df.groupby(by='Country name')
 country.size()
 
@@ -27,10 +42,6 @@ ind=df['christelle'].idxmax()
 df.loc[ind]
 
 
-# -
-
 maxi=df['christelle'].max()
 mini=df['christelle'].min()
 print(maxi,mini)
-
-nouvelle_col=pd.cut('christelle',bins=
