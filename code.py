046672a570579a0aece2698b0fcedbf5937976e3
Pierre-Louis COPPENS <pierre-louis.coppens@etu.minesparis.psl.eu>
2024-10-29 15:11:46 +0100

    Mise en page et importation

--- code.py
+++ code.py
@@ -1,1 +1,15 @@
+# # Projet Hackathon - Christelle, Maxime, Jérémie, Pierre-Louis
+#
+# * Projet choisi _Le bonheur_
+# * Objectif : définition d'un nouvelle indice de bonheur
+
+# ## Importation des librairies
+
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import numpy as np
+import common as c
+
+
+
 
