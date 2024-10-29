dd7d58bb76389eed7e65284071b2ecb97844433e
maximebailly <maxime.bailly2402@gmail.com>
2024-10-29 15:18:34 +0100

    code.py avec GDP normalised

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('bonheur.csv', sep=';')
+df.head(2)
+
+df['GDP Normalisé'] = df['Log GDP per capita']/(df.loc[df['Log GDP per capita'].argmax(), 'Log GDP per capita'])
+df.head(3)
+
 
