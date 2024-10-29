41e2fa39d57394a19051914773fae3ec0faca81d
Pierre-Louis COPPENS <pierre-louis.coppens@etu.minesparis.psl.eu>
2024-10-29 15:08:28 +0100

    Mise en forme initiale de code

--- code.py
+++ code.py
@@ -1,1 +1,13 @@
+# # Projet Hackathon - Christelle, Maxime, Jérémie, Pierre-Louis
+#
+# * Projet choisi _Le bonheur_
+# * Objectif : définition d'un nouvelle indice de bonheur
+
+# ## Importation des librairies
+
+import common.py as c
+import pandas as pd 
+import numpy as np
+import matplolib.pyplot as plt
+
 
